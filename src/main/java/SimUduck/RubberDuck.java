75e199a8712899ed71ef9f37c65b257b5fe2f6a9
Donghee-Jeong <weg9511@gmail.com>
2023-11-20 23:23:33 +0900

    1. 심각한 문제 발생
    Duck에 fly()만 추가했는데 고무 오리가 날아다니는 오류가 발생

---
+++ src/main/java/SimUduck/RubberDuck.java
@@ -0,0 +1,14 @@
+package SimUduck;
+
+public class RubberDuck extends Duck {
+
+    @Override
+    public void quack() {
+        System.out.println("삑삑!");
+    }
+
+    @Override
+    public void display() {
+        // 적당한 모양을 표시
+    }
+}
