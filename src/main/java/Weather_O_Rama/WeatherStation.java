2682c6288c69128566157d7cefc48d3e8b981df5
Donghee-Jeong <weg9511@gmail.com>
2023-11-27 22:37:51 +0900

    2. 기상 스테이션 테스트

---
+++ src/main/java/Weather_O_Rama/WeatherStation.java
@@ -0,0 +1,14 @@
+package Weather_O_Rama;
+
+import Weather_O_Rama.display.CurrentConditionsDisplay;
+
+public class WeatherStation {
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+    }
+}
