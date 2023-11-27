41b2aacc2994e04135167b20c2bab10fc2e6eb06
Donghee-Jeong <weg9511@gmail.com>
2023-11-27 21:40:35 +0900

    2. WeatherData 클래스

---
+++ src/main/java/Weather_O_Rama/WeatherData.java
@@ -0,0 +1,18 @@
+package Weather_O_Rama;
+
+public class WeatherData {
+
+    // getTemperature()
+    // getHumidity()
+    // getPressure()
+    // measurementsChanged()
+
+    /**
+     * 기상 관측값이
+     * 갱신될 때마다
+     * 이 메소드가 호출됩니다
+     */
+    public void measurementsChanged() {
+        // 코드가 들어갈 자리
+    }
+}
