431e421c0e28aebc7466f43a155b6066d38cb7e2
Donghee-Jeong <weg9511@gmail.com>
2023-11-27 22:29:07 +0900

    2. 디스플레이 요소 구현
    - update메서드에서 display메서드를 호출하는 방식은 최선이 아니다.
    - weatherData(주제 레퍼런스)를 미리 저장해두면 나중에 탈퇴할 때 유용하게 쓸 수 있다.

---
+++ src/main/java/Weather_O_Rama/display/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package Weather_O_Rama.display;
+
+import Weather_O_Rama.DisplayElement;
+import Weather_O_Rama.Observer;
+import Weather_O_Rama.WeatherData;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("온도 : " + temperature + ", 습도 : " + humidity);
+    }
+}
---
+++ src/main/java/Weather_O_Rama/display/StatisticsDisplay.java
@@ -0,0 +1,16 @@
+package Weather_O_Rama.display;
+
+import Weather_O_Rama.DisplayElement;
+import Weather_O_Rama.Observer;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
