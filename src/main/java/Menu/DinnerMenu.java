55374d09fa6a42dca14279ee4c2367dddddda8f5
Donghee-Jeong <weg9511@gmail.com>
2023-12-04 22:00:21 +0900

    9. 서로 다른 메뉴 구현법

---
+++ src/main/java/Menu/DinnerMenu.java
@@ -0,0 +1,35 @@
+package Menu;
+
+public class DinnerMenu {
+    static final int MAX_ITEMS = 2;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public DinnerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("BLT",
+                "통밀 위에 베이컨, 상추, 토마토를 얹은 메뉴",
+                false,
+                2.99);
+
+        addItem("오늘의  스프",
+                "감자 샐러드를 곁들인 오늘의 스프",
+                false,
+                3.29);
+    }
+
+    private void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if (numberOfItems >= MAX_ITEMS) {
+            System.out.println("죄송합니다, 메뉴가 꽉 찼습니다. 더 이상 추가할 수 없습니다.");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems++;
+        }
+    }
+
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+}
---
+++ src/main/java/Menu/PancakeHouseMenu.java
@@ -0,0 +1,30 @@
+package Menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeHouseMenu {
+    List<MenuItem> menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList<>();
+
+        addItem("블루베리 팬케이크",
+                "신선한 블루베리와 블루베리 시럽으로 만든 팬케이크",
+                true,
+                3.49);
+
+        addItem("와플",
+                "취향에 따라 블루베리나 딸기를 얹을 수 있는 와플",
+                true,
+                3.59);
+    }
+
+    private void addItem(String name, String description, boolean vegetarian, double price) {
+        menuItems.add(new MenuItem(name, description, vegetarian, price));
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
