dfea4b3388324e11fe6c6b7c121ee9b9019a11d1
Donghee-Jeong <weg9511@gmail.com>
2023-12-04 23:02:23 +0900

    9. 종업원 코드 개선

--- src/main/java/Menu/MenuTestDrive.java
+++ src/main/java/Menu/MenuTestDrive.java
@@ -1,12 +1,14 @@
 package Menu;
 
+import java.util.List;
+
 public class MenuTestDrive {
     public static void main(String[] args) {
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinnerMenu dinnerMenu = new DinnerMenu();
         CafeMenu cafeMenu = new CafeMenu();
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinnerMenu, cafeMenu);
+        Waitress waitress = new Waitress(List.of(pancakeHouseMenu, dinnerMenu, cafeMenu));
         waitress.printMenu();
     }
 }
--- src/main/java/Menu/Waitress.java
+++ src/main/java/Menu/Waitress.java
@@ -2,27 +2,21 @@ package Menu;
 
 
 import java.util.Iterator;
+import java.util.List;
 
 public class Waitress {
+    List<Menu> menus;
 
-    Menu pancakeHouseMenu;
-    Menu dinnerMenu;
-    Menu cafeMenu;
-
-    public Waitress(Menu pancakeHouseMenu, Menu dinnerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinnerMenu = dinnerMenu;
-        this.cafeMenu = cafeMenu;
+    public Waitress(List<Menu> menus) {
+        this.menus = menus;
     }
 
     public void printMenu() {
-        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator<MenuItem> dinnerIterator = dinnerMenu.createIterator();
-        Iterator<MenuItem> cafeMenuIterator = cafeMenu.createIterator();
-
-        printMenu(pancakeIterator);
-        printMenu(dinnerIterator);
-        printMenu(cafeMenuIterator);
+        Iterator<Menu> menuIterator = menus.iterator();
+        while (menuIterator.hasNext()) {
+            Menu menu = menuIterator.next();
+            printMenu(menu.createIterator());
+        }
     }
 
     private void printMenu(Iterator<MenuItem> iterator) {
