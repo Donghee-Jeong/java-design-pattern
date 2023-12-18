f07ed3606224d628b7483be19d85d34e584b4e53
Donghee-Jeong <weg9511@gmail.com>
2023-12-18 22:55:50 +0900

    12. 오리가 소리 낸 횟수

--- src/main/java/UpgradeDuck/DuckSimulator.java
+++ src/main/java/UpgradeDuck/DuckSimulator.java
@@ -7,11 +7,11 @@ public class DuckSimulator {
     }
 
     private void simulate() {
-        Quackable mallarDuck = new MallarDuck();
-        Quackable redheadDuck = new RedheadDuck();
-        Quackable duckCall = new DuckCall();
-        Quackable rubberDuck = new RubberDuck();
-        Quackable gooseDuck = new GooseAdapter(new Goose());
+        Quackable mallarDuck = new QuackCounter(new MallarDuck());
+        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+        Quackable duckCall = new QuackCounter(new DuckCall());
+        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+        Quackable gooseDuck = new QuackCounter(new GooseAdapter(new Goose()));
 
         System.out.println("\n오리 시뮬레이션 게임");
 
@@ -20,6 +20,8 @@ public class DuckSimulator {
         simulate(duckCall);
         simulate(rubberDuck);
         simulate(gooseDuck);
+
+        System.out.println("오리가 소리 낸 횟수 : " + QuackCounter.getQuack() + " 번");
     }
 
     void simulate(Quackable duck) {
---
+++ src/main/java/UpgradeDuck/QuackCounter.java
@@ -0,0 +1,20 @@
+package UpgradeDuck;
+
+public class QuackCounter implements Quackable {
+    private Quackable duck;
+    private static int numberOfQuacks;
+
+    public QuackCounter(Quackable duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public void quack() {
+        duck.quack();
+        numberOfQuacks++;
+    }
+
+    public static int getQuack() {
+        return numberOfQuacks;
+    }
+}
